ecfed1f523065c1810b6e4c6de954664af7810ef
Corentin Pane <corentin.pane@eatops.com>
2022-03-04 13:07:07 +0100

    Solve issue with update to 1.7.4

--- src/AmlEngineImportBug.Library/ImportInstanceHierarchy.cs
+++ src/AmlEngineImportBug.Library/ImportInstanceHierarchy.cs
@@ -4,9 +4,9 @@ using Aml.Engine.CAEX.Extensions;
 namespace AmlEngineImportBug.Library {
     public static class ImportInstanceHierarchy {
         public static void DoStuff(string amlPath) {
-            CAEXDocument originalDocument = CAEXDocument.LoadFromFile(amlPath);
+            using CAEXDocument originalDocument = CAEXDocument.LoadFromFile(amlPath);
             var ie = originalDocument.CAEXFile.InstanceHierarchy;
-            var document = CAEXDocument.LoadFromFile("IEC63131AmlLibrary0.0.9.aml");
+            using var document = CAEXDocument.LoadFromFile("IEC63131AmlLibrary0.0.9.aml");
             foreach (var item in ie) {
                 document.CAEXFile.Import_InstanceHierarchy(item);
             }
